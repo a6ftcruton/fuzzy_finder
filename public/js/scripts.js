a8b6f3b427903cb63124f52252a70b5e6dcdcf09
Jim Sutton <jimsuttonjimsutton@gmail.com>
2015-02-05 16:40:49 -0700

    Add google font for make pretties

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
   file = "http://localhost:4567/lib/urls.txt"
-  $.get(file, function(data){
-    console.log(data)
+
+  $.get(file, function(data) {
+    console.log(data);
   });
+
   $('#search-bar').keyup(function(data) {
     $('.results').append($('#search-bar').val());
   });
