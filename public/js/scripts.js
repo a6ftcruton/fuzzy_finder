e15613dfb875614f30dba60812ae0a2d94fc96aa
Jim Sutton <jimsuttonjimsutton@gmail.com>
2015-02-05 17:12:56 -0700

    Fix text results display

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
-  file = "http://localhost:4567/lib/urls.txt"
+  var file = "http://localhost:4567/lib/urls.txt";
 
   $.get(file, function(data) {
     console.log(data);
+    var urls = data;
   });
 
   $('#search-bar').keyup(function(data) {
-    $('.results').append($('#search-bar').val());
+    $('.results').empty().append($('#search-bar').val());
   });
 });
+
+// load the results of the file, then split them on the newline character
+// on first page load, display first 10 urls, create an li  (use underscore)
+// as user types,display only those urls that contain those letters in that order 
+//   to use a regexp: /a.*b.*c/   where a b and c are the letters typed...
