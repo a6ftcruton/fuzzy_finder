dce80fafe427cebd0d72ebd08523af46211a6bb5
Jim Sutton <jimsuttonjimsutton@gmail.com>
2015-02-19 13:52:20 -0700

    Display number of potential matches

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -9,16 +9,19 @@ $(document).ready(function() {
 
   $('#search-bar').keyup(function() {
     var counter = 0;
+    var matches = 0;
     $('#file').empty();
+
     var userSearch = $('#search-bar').val().split("").join(".*");
-    $('.results').empty().append($('#search-bar').val());
 
     urls.forEach(function(url) {
-      if (counter < 15) {
         if( url.match(userSearch) ) {
+          matches++; 
           $('#file').append('<li class="url-result"><a href="' + url + '">' + url + '</a></li>');
+          $('#result-count').text(matches + " potential matches");
           counter++;
-        }
+      } else {
+          $('#result-count').text(matches + " potential matches");
       }
     });
   });
